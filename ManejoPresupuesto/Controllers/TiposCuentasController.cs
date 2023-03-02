ebcb1b42ccf86fe90c98746f5426c7d0f86e9958
fanny00 <fannycamposa@gmail.com>
2023-03-02 18:50:18 -0300

    Insertando un tipo de cuenta en la BD - Error

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 
@@ -8,16 +9,14 @@ namespace ManejoPresupuesto.Controllers
     public class TiposCuentasController: Controller 
     {
         private readonly string connectionString;
-        public TiposCuentasController(IConfiguration configuration)
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
         {
-            connectionString = configuration.GetConnectionString("DefaultConnection");
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
         }
         public IActionResult Crear()
         {
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var query = connection.Query("SELECT 1").FirstOrDefault();
-            }
                 return View();
         }
 
@@ -28,6 +27,9 @@ namespace ManejoPresupuesto.Controllers
             {
                 return View(tipoCuenta);
             }
+
+            tipoCuenta.UsuarioId = 1;
+            repositorioTiposCuentas.crear(tipoCuenta);
             return View();
         }
 
---
+++ ManejoPresupuesto/Servicios/RepositoriosTiposCuentas.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTiposCuentas
+    {
+        void crear(TipoCuenta tipoCuenta);
+    }
+    public class RepositoriosTiposCuentas
+    {
+        private readonly string connectionString;
+        public RepositoriosTiposCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public void Crear(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = connection.QuerySingle<int>($@"INSERT INTO TipoCuentas (Nombre, UsuarioId, Orden)
+                                                    VALUES (@Nombre, @UsuarioId, 0);
+                                                    SELECT SCOPE_IDENTITY();", tipoCuenta);
+            tipoCuenta.Id = id;
+        }
+    }
+}
