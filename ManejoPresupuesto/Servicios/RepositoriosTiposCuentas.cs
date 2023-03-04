4aef93227cc89d4ee1a308c6b95d7ce6bac3e78e
fanny00 <fannycamposa@gmail.com>
2023-03-03 21:08:45 -0300

    Validaciones Personalizadas a Nivel del Controlador

--- ManejoPresupuesto/Servicios/RepositoriosTiposCuentas.cs
+++ ManejoPresupuesto/Servicios/RepositoriosTiposCuentas.cs
@@ -7,6 +7,7 @@ namespace ManejoPresupuesto.Servicios
     public interface IRepositorioTiposCuentas
     {
         Task Crear(TipoCuenta tipoCuenta);
+        Task<bool> Existe(string nombre, int usuarioId);
     }
     public class RepositoriosTiposCuentas : IRepositorioTiposCuentas
     {
@@ -20,10 +21,21 @@ namespace ManejoPresupuesto.Servicios
         {
             using var connection = new SqlConnection(connectionString);
             var id = await connection.QuerySingleAsync<int>
-                                                  ($@"INSERT INTO TipoCuentas (Nombre, UsuarioId, Orden)
+                                                  (@"INSERT INTO TipoCuentas (Nombre, UsuarioId, Orden)
                                                     VALUES (@Nombre, @UsuarioId, 0);
                                                     SELECT SCOPE_IDENTITY();", tipoCuenta);
             tipoCuenta.Id = id;
+        }
+
+        public async Task<bool> Existe(string nombre, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var existe = await connection.QueryFirstOrDefaultAsync<int>(
+                                @"SELECT 1 
+                                FROM TipoCuentas
+                                WHERE Nombre = @Nombre AND UsuarioId = @UsuarioId;",
+                                new {nombre, usuarioId});
+            return existe == 1;
         }
     }
 }
